c98c5849fad31b66eedaa23ce2ba78e57a1a3ed8
KYIV\Maryana_Kuzhel <maryana_kuzhel@epam.com>
2021-11-29 09:26:14 +0200

    organize folder structure

--- Task06_XML_JSON_LibraryDB/HomeTask06.Halyna_Chervoniak.sql
+++ Task06_XML_JSON_LibraryDB/HomeTask06.Halyna_Chervoniak.sql
@@ -1,8 +1,28 @@
-﻿--Subtask.05.01 
---Topic: Returning Results as XML with FOR XML Task: Get customers who made orders, number of orders and order date.
---Don't present customers without orders. Use Sales.Customer, Sales.SalesOrderHeader, Person.Person and FOR XML PATH.
+﻿use AdventureWorks2019;
+go
+--     Subtask.05.01 	Topic: Returning Results as XML with FOR XML  
+/*
+Task: Get customers who made orders, number of orders and order date. 
+Don't present customers without orders. 
+Use Sales.Customer, Sales.SalesOrderHeader, Person.Person and FOR XML PATH. 
+Expected result: 
+<Customer custid="11000" name="Mary C Young"> 
+     <Order orderNumber="SO43793" orderdate="2011-06-21T00:00:00" /> 
+     <Order orderNumber="SO51522" orderdate="2013-06-20T00:00:00" /> 
+     <Order orderNumber="SO57418" orderdate="2013-10-03T00:00:00" /> 
+</Customer> 
+… 
+ 
+ 
+<Customer custid="11000" name="Jon V Yang"> 
+  <Order orderNumber="SO43793" orderdate="2011-06-21T00:00:00" /> 
+  <Order orderNumber="SO51522" orderdate="2013-06-20T00:00:00" /> 
+  <Order orderNumber="SO57418" orderdate="2013-10-03T00:00:00" /> 
+</Customer> 
+… 
+*/
 
----Subtask.05.01 option 1
+-- option 1
 select Header.[CustomerID],
        Header.OrderDate, 
        Header.[SalesOrderID]
@@ -20,7 +40,7 @@ FOR XML PATH ('Customer'),  ROOT('Customer')
 --FOR XML RAW;
 --FOR XML AUTO;
 
----Subtask.05.01 option 2 NESTED
+-- option 2 NESTED
 select Customer.[CustomerID] AS [@custid],
       p.[FirstName] +' ' + ISNULL(p.[MiddleName],' ') +' ' + p.[LastName] AS [@Name],
       ( 
@@ -42,8 +62,12 @@ ORDER BY Customer.[CustomerID]
 FOR XML PATH ('Customer'),  ROOT('Customer')
 
 
---Subtask.05.02  Get set of products, price and quantity from file “Subtask03.txt”. 
-DECLARE @DocHandle AS INT; 
+--     Subtask.05.02	Topic: Querying and Managing XML Data   
+/*Task: Get set of products, price and quantity from file “Subtask03.txt”. 
+
+Expected result: 
+ */
+ DECLARE @DocHandle AS INT; 
 DECLARE @XmlDocument AS NVARCHAR(1000); 
 SET @XmlDocument = N' 
 <products>
@@ -132,14 +156,16 @@ GO
 				<sum>33900</sum>
 			</product>*/
 
-/*Subtask.05.03	Topic: Querying and Managing XML Data
-Task: Get list of ProductName as a single line separated by commas, and a separator in alphabetical order. 
-Querying and Managing XML Data Get list of Products as a single line separated by commas, and a separator in alphabetical order
-Use [Production].[Product]. 
-Expected result:
-'Adjustable Race', 'All-Purpose Bike Stand', 'AWC Logo Cap', 'BB Ball Bearing', 'Bearing Ball', ... */
 
------Subtask.05.03 option 1 FOR XML PATH
+--     Subtask.05.03	Topic: Querying and Managing XML Data
+/*Task: Get list of ProductName as a single line separated by commas, and a separator in alphabetical order. 
+Use [Production].[Product]. 
+
+Expected result:
+'Adjustable Race', 'All-Purpose Bike Stand', 'AWC Logo Cap', 'BB Ball Bearing', 'Bearing Ball', ... 
+*/
+
+-- option 1 FOR XML PATH
 SELECT TOP(1) SUBSTRING(
     (
         SELECT ',' + [Name]
@@ -147,24 +173,24 @@ SELECT TOP(1) SUBSTRING(
         ORDER BY [Name]
         FOR XML PATH('')), 2, 200000)
  AS ProductName
-FROM [Production].[Product] t
+FROM [Production].[Product] t;
 
 SELECT [Name] 
 FROM [Production].[Product]
-ORDER  BY [Name] ASC
+ORDER  BY [Name] ASC;
 
------Subtask.05.03 option 2 STRING_AGG
+-- option 2 STRING_AGG
 
 SELECT 
       STRING_AGG (CAST ([Name] AS NVARCHAR(MAX)),',') AS Result 
-FROM [Production].[Product]
+FROM [Production].[Product];
 
 SELECT 
-      STRING_AGG (CAST ([Name] AS NVARCHAR(MAX)),',')   WITHIN GROUP ( ORDER BY [Name] ASC)  AS Result FROM [Production].[Product]
+      STRING_AGG (CAST ([Name] AS NVARCHAR(MAX)),',')   WITHIN GROUP ( ORDER BY [Name] ASC)  AS Result FROM [Production].[Product];
 
-/*Subtask.05.04	Topic: Convert SQL Server data to JSON or export JSON
-Task: Create a script which will return JSON based on data in  [Production].[Product]
 
+--Subtask.05.04	Topic: Convert SQL Server data to JSON or export JSON
+/*Task: Create a script which will return JSON based on data in  [Production].[Product]
 Return following JSON for all users:
 [
 	{
@@ -192,22 +218,24 @@ SELECT[ProductID]
 FROM [Production].[Product]
 FOR JSON AUTO;
 
+
 --Subtask.05.05	Topic: Extract values from JSON text and use them in queries
---Task: Create a script which will return table dataset from following JSON
---
---[{"UserID":1,"UserName":"Andrey Potapov","Email":"Andrey.Potapov@insidemedia.onmicrosoft.com",
---"Flags":1,"Notes":"","Type":1,"DisplayName":"Andrey Potapov","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":2,"UserName":"Michail Kovalenko","Email":"michail.kovalenko@insidemedia.onmicrosoft.com","Flags":1,"Notes":"","Type":1,"DisplayName":"Michail Kovalenko","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":5,"UserName":"Anton Belousov","Email":"Anton.Belousov@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Anton Belousov","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":6,"UserName":"Arturo Morales","Email":"Arturo.Morales@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Arturo Morales","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":7,"UserName":"Bruno Silveira","Email":"bruno.silveira@groupm.com","Flags":1,"Type":1,"DisplayName":"Bruno Silveira","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":8,"UserName":"Daniel Escribano","Email":"Daniel.Escribano@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Daniel Escribano","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":9,"UserName":"Dariia Vasilenko","Email":"dariia.vasilenko@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Dariia Vasilenko","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":10,"UserName":"Darya Pivikova","Email":"darya.pivikova@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Darya Pivikova","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":11,"UserName":"Dmitri Sedin","Email":"Dmitri.Sedin@insidemedia.onmicrosoft.com"}] 
--- 
---Display following column 
---[ID]
---[UserName]
---[ActualEmail]
---[Email]
---[Flags]
---[Notes]
---[Type]
---[DisplayName]
---[CreatedDate]
+/*Task: Create a script which will return table dataset from following JSON
+
+[{"UserID":1,"UserName":"Andrey Potapov","Email":"Andrey.Potapov@insidemedia.onmicrosoft.com","Flags":1,"Notes":"","Type":1,"DisplayName":"Andrey Potapov","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":2,"UserName":"Michail Kovalenko","Email":"michail.kovalenko@insidemedia.onmicrosoft.com","Flags":1,"Notes":"","Type":1,"DisplayName":"Michail Kovalenko","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":5,"UserName":"Anton Belousov","Email":"Anton.Belousov@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Anton Belousov","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":6,"UserName":"Arturo Morales","Email":"Arturo.Morales@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Arturo Morales","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":7,"UserName":"Bruno Silveira","Email":"bruno.silveira@groupm.com","Flags":1,"Type":1,"DisplayName":"Bruno Silveira","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":8,"UserName":"Daniel Escribano","Email":"Daniel.Escribano@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Daniel Escribano","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":9,"UserName":"Dariia Vasilenko","Email":"dariia.vasilenko@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Dariia Vasilenko","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":10,"UserName":"Darya Pivikova","Email":"darya.pivikova@insidemedia.onmicrosoft.com","Flags":1,"Type":1,"DisplayName":"Darya Pivikova","sysinfo":{"CreatedAt":"2018-01-29T15:06:43.3300000","ChangedAt":"2018-01-29T15:06:43.3300000"}},{"UserID":11,"UserName":"Dmitri Sedin","Email":"Dmitri.Sedin@insidemedia.onmicrosoft.com"}] 
+ 
+Display following column 
+[ID]
+[UserName]
+[ActualEmail]
+[Email]
+[Flags]
+[Notes]
+[Type]
+[DisplayName]
+[CreatedDate]
+[LastModifiedDate]
+*/
 
 ---Subtask.05.05 option 1
 DECLARE @json AS NVARCHAR(MAX) = N'
@@ -254,6 +282,5 @@ DECLARE @JSON NVARCHAR(MAX) = N'[
  ]'; 
 SELECT root.[key] AS [Order],TheValues.[key], TheValues.[value]
 FROM OPENJSON ( @JSON ) AS root
-CROSS APPLY OPENJSON ( root.value) AS TheValues
-
+CROSS APPLY OPENJSON ( root.value) AS TheValues;
 
